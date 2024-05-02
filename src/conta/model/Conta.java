2044dc1fcd2e6aa1581872c07cfa29bf7d4c3826
Leonardo Gobetti <leonardogobetti@hotmail.com>
2024-05-02 11:53:31 -0300

    Classe Conta Corrente e Classe Conta Poupança

--- src/conta/model/Conta.java
+++ src/conta/model/Conta.java
@@ -8,7 +8,6 @@ public class Conta {
 	private String titular;
 	private float saldo;
 	
-	
 	public Conta(int numero, int agencia, int tipo, String titular, float saldo) {
 		this.numero = numero;
 		this.agencia = agencia;
@@ -22,46 +21,37 @@ public class Conta {
 		return numero;
 	}
 
-
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
-
 
 	public int getAgencia() {
 		return agencia;
 	}
 
-
 	public void setAgencia(int agencia) {
 		this.agencia = agencia;
 	}
-
 
 	public int getTipo() {
 		return tipo;
 	}
 
-
 	public void setTipo(int tipo) {
 		this.tipo = tipo;
 	}
-
 
 	public String getTitular() {
 		return titular;
 	}
 
-
 	public void setTitular(String titular) {
 		this.titular = titular;
 	}
 
-
 	public float getSaldo() {
 		return saldo;
 	}
-
 
 	public void setSaldo(float saldo) {
 		this.saldo = saldo;
