fc8a6b650297d5c947ddc0e678441c911770bfc0
Leonardo Gobetti <leonardogobetti@hotmail.com>
2024-05-02 14:53:26 -0300

    Interface ContaRepository e mudança para abstract na Classe Conta

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -2,8 +2,6 @@ package conta;
 
 import java.util.Scanner;
 
-
-import conta.model.Conta;
 import conta.model.ContaCorrente;
 import conta.model.ContaPoupanca;
 import conta.util.Cores;
@@ -11,14 +9,6 @@ import conta.util.Cores;
 public class Menu {
 
 	public static void main(String[] args) {
-		
-		//Teste da Classe Conta
-		Conta c1 = new Conta(1, 123, 1, "Leonardo", 10000.0f);
-		c1.visualizar();
-		c1.sacar(12000.0f);
-		c1.visualizar();
-		c1.depositar(5000.0f);
-		c1.visualizar();
 		
 		// Teste da Classe Conta Corrente
 		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "Mariana", 15000.0f, 1000.0f);
