c3c2fdefb051ac90e9faca7f721d3dd58e86bca0
Leonardo Gobetti <leonardogobetti@hotmail.com>
2024-05-03 13:07:55 -0300

    Tratando Exceptions na Classe Menu e método KeyPress

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,5 +1,8 @@
 package conta;
 
+import java.io.IOException;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import conta.model.ContaCorrente;
@@ -53,8 +56,13 @@ public class Menu {
 			System.out.println("Entre com a opção desejada:                          ");
 			System.out.println("                                                     " + Cores.TEXT_RESET);
 
-			opcao = leia.nextInt();
-			
+			try {
+				opcao = leia.nextInt();
+			}catch(InputMismatchException e) {
+				System.out.println("\nDigite Valores inteiros!");
+				leia.nextLine();
+				opcao = 0;
+			}
 			
 			if(opcao == 9) {
 				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil Com Z- O seu Futuro começa aqui!");
@@ -66,37 +74,46 @@ public class Menu {
 			case 1:
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Criar Conta\n\n");
 				
+				keyPress();
 				break;
 			case 2:
 				System.out.println(Cores.TEXT_WHITE_BOLD +"Listar todas as Contas\n\n");
 				
+				keyPress();
 				break;
 			case 3:
 				System.out.println(Cores.TEXT_WHITE_BOLD +"Consultar dados da Conta - por número\n\n");
 				
+				keyPress();
 				break;
 			case 4:
 				System.out.println(Cores.TEXT_WHITE_BOLD +"Atualizar dados da Conta\n\n");
 				
+				keyPress();
 				break;
 			case 5:
 				System.out.println(Cores.TEXT_WHITE_BOLD +"Apagar conta\n\n");
 				
+				keyPress();
 				break;
 			case 6:
 				System.out.println(Cores.TEXT_WHITE_BOLD +"Saque\n\n");
 				
+				keyPress();
 				break;
 			case 7:
 				System.out.println(Cores.TEXT_WHITE_BOLD +"Depósito\n\n");
 				
+				keyPress();
 				break;
 			case 8:
 				System.out.println(Cores.TEXT_WHITE_BOLD +"Transferência entre Contas\n\n");
 				
+				keyPress();
 				break;
 			default:
-				System.out.println(Cores.TEXT_RED_BOLD+ "Opção Inválida!\n"+ Cores.TEXT_RESET);
+				System.out.println(Cores.TEXT_RED_BOLD+ "\nOpção Inválida!"+ Cores.TEXT_RESET);
+				keyPress();
 				break;
 			}
 		}
@@ -109,6 +126,17 @@ public class Menu {
 		System.out.println("https://github.com/Leogb2014");
 		System.out.println("*********************************************************");
 		
+	}
+	
+	public static void keyPress() {
+		
+		try {
+			System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para Continuar...");
+			System.in.read();
+		}catch(IOException e) {
+			
+			System.out.println("Você pressionou uma tecla diferente de enter!");
+		}
 	}
 
 	}
